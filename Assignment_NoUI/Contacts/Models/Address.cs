39354290ff0a6739d83962a85bcb530d36f37a4b
Anton Marijanovic <anton_m00@hotmail.com>
2023-10-12 12:13:37 +0200

    update

--- Assignment_NoUI/Contacts/Models/Address.cs
+++ Assignment_NoUI/Contacts/Models/Address.cs
@@ -2,10 +2,10 @@
 
 public class Address
 {
-    public string StreetName { get; set; }
-    public string StreetNumber { get; set; }
-    public string PostalCode { get; set; }
-    public string City { get; set; }
+    public string? StreetName { get; set; }
+    public string? StreetNumber { get; set; }
+    public string? PostalCode { get; set; }
+    public string? City { get; set; }
 
     public string FullAddress => $"{StreetName} {StreetNumber}\n{PostalCode} {City}";
 }
--- Assignment_NoUI/Contacts/Models/Contact.cs
+++ Assignment_NoUI/Contacts/Models/Contact.cs
@@ -4,10 +4,10 @@ namespace Contacts.Models;
 
 public class Contact : IContact
 {
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Email { get; set; }
-    public string PhoneNumber { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Email { get; set; }
+    public string? PhoneNumber { get; set; }
     public IAddress Address { get; set; }
-    public string FullName => $"{FirstName} {LastName}";
+    public string? FullName => $"{FirstName} {LastName}";
 }
