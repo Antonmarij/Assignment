de1918aed3028fbc0a6ee2deaf154f4e39834d11
Anton Marijanovic <anton_m00@hotmail.com>
2023-10-16 08:49:28 +0200

    done

--- Assignment_NoUI/Contacts/Services/FileService.cs
+++ Assignment_NoUI/Contacts/Services/FileService.cs
@@ -2,6 +2,7 @@
 
 public class FileService
 {
+    //sparar till fil som den ska men skriver över varje gång, kanske inte spelar roll?
     public static void SaveToFile(string filePath, string content)
     {
         using StreamWriter sw = new StreamWriter(filePath);
